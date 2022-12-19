c771db4aa0ebdb8ce8bccb419ed6b6f0ac454b83
Arsibal97 <asierralaya@gmail.com>
2022-12-19 13:49:11 +0100

    segundo

--- src/p31/ejemplo.java
+++ src/p31/ejemplo.java
@@ -3,7 +3,8 @@ package p31;
 public class ejemplo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hola");
+		System.out.println("adios");
 
 	}
 
